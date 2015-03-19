d650f41a198491426cb4439717c0309a1767f851
Paul <herppfel@gmail.com>
2015-03-19 15:21:18 -0400

    more methods + test page

--- util.js
+++ util.js
@@ -1,11 +1,14 @@
 /**
-* Written by @Pinjasaur.
+* Keyboarded by @Pinjasaur.
 *
-* Last updated 3/15/2015.
+* Thrown together with tears of dispair, coffee, StackOverflow, and other interwebs thingies.
+*
+* Long live #LordGaben.
 */
 (function(global) {
   var Util = {
     version: "0.0.1",
+    author: "@Pinjasaur",
     rot13: function(str) {
       return (str + "").replace(/[a-z]/gi, function(s) {
           return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < "n" ? 13 : -13));
@@ -13,6 +16,23 @@
     },
     squish: function(str) {
       return str.replace(/\s+/g, "");
+    },
+    engrishify: function(str) {
+      return str.trim().replace(/\s+/g, " ");
+    },
+
+    /**
+    * Acquired from http://www.paulirish.com/2009/random-hex-color-code-snippets/.
+    */
+    randHex: function() {
+      return "#" + Math.floor(Math.random()*16777216).toString(16);
+    },
+    randInt: function(min, max) {
+      if (min && typeof min === "number" && max && typeof max === "number") {
+        return Math.floor(Math.random()*(max - min + 1) + min);
+      } else {
+        throw new Error("Missing parameter(s) or parameter(s) are of wrong data type.")
+      }
     }
   }
 
